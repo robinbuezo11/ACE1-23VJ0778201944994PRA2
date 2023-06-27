622f59bbe89ce34c923a1b59f3a9feb47b76080e
robinbuezo11 <3384471212007@ingenieria.usac.edu.gt>
2023-06-27 02:34:23 -0600

    [add] delete product

--- Program.asm
+++ Program.asm
@@ -12,13 +12,19 @@ new_line        db  0a, "$"
 in_buffer       db  20, 00
                 db  20 dup (0)
 number          db  05 dup (30)
+exit_option     db  "(S)alir", 0a, "$"
+back_option     db  "(R)egresar", 0a, "$"
+
+zeros           db  28 dup (0)
+
+pointer_tmp     dw  0000
 
 ; Header
 usac            db  "Universidad de San Carlos de Guatemala", 0a, "$"
 facul           db  "Facultad de Ingenieria", 0a, "$"
 school          db  "Escuela de Vacaciones", 0a, "$"
 course          db  "Arquitectura de Computadores y Ensambladores 1", 0a, "$"
-student         db  "Robin Omar Buezo Díaz", 0a, "$"
+student         db  "Robin Omar Buezo Diaz", 0a, "$"
 
 ; Login
 username        db  "Nombre: ", "$"
@@ -55,19 +61,23 @@ products_delete_msg     db  "(E)liminar producto", 0a, "$"
 products_show_msg       db  "(M)ostrar productos", 0a, "$"
 products_file           db  "PROD.BIN", 00
 handle_products         dw  0000
+product_not_found       db  "Producto no encontrado", 0a, "$"
+product_deleted         db  "Producto eliminado exitosamente", 0a, "$"
+product_exists_msg      db  "El producto ya existe", 0a, "$"
 
 ; Product Structure
-product_code            db  05 dup (0)
-product_desc            db  21 dup (0)
+product_code            db  04 dup (0)
+product_desc            db  20 dup (0)
 product_price           db  05 dup (0)
 product_stock           db  05 dup (0)
 req_product             db  "Ingrese los datos del producto: ", 0a, "$"
-req_code                db  "Codigo: ", 00
-req_desc                db  "Descripcion: ", 00
-req_price               db  "Precio: ", 00
-req_stock               db  "Unidades: ", 00
-product_num_price       dw 0000
-product_num_stock       dw 0000
+req_code                db  "Codigo: ", "$"
+req_desc                db  "Descripcion: ", "$"
+req_price               db  "Precio: ", "$"
+req_stock               db  "Unidades: ", "$"
+product_num_price       dw  0000
+product_num_stock       dw  0000
+product_tmp             db  28 dup (0)
 
 
 .CODE
@@ -136,26 +146,26 @@ copy_string: ;------------------------------------Start of copy_string
         ret
         ;------------------------------------End of copy_string
 
-string_to_int: ;----------------------------------Start of string_to_int
+string_to_num: ;----------------------------------Start of string_to_num
         ; Convert string to int
         ; Input: DI = string
         ; Output: AX = int
         mov AX, 0000
         mov CX, 0005
-cycle_string_to_int:
+cycle_string_to_num:
         mov BL, [DI]
         cmp BL, 00
-        je end_string_to_int
+        je end_string_to_num
         sub BL, 30
         mov DX, 000a
         mul DX
         mov BH, 00
         add AX, BX
         inc DI
-        loop cycle_string_to_int
-end_string_to_int:
+        loop cycle_string_to_num
+end_string_to_num:
         ret
-        ;----------------------------------End of string_to_int
+        ;----------------------------------End of string_to_num
 
 int_to_string: ;------------------------------------Start of int_to_string
         ; Convert int to string
@@ -202,9 +212,10 @@ memset: ;----------------------------------------------Start of memset
         ; Input: DI = memory
         ;        CX = length
         ;        AL = value
+cycle_memset:
         mov [DI], AL
         inc DI
-        loop memset
+        loop cycle_memset
         ret
         ;----------------------------------------------End of memset
 
@@ -438,6 +449,7 @@ principal_menu: ;----------------------------------------------Start of principa
         print products
         print sales
         print utils
+        print exit_option
         call get_pressed_key
         cmp AL, 50 ;------------------ Products
         je products_menu
@@ -451,6 +463,10 @@ principal_menu: ;----------------------------------------------Start of principa
         je utils_menu
         cmp AL, 68 ;------------------ Utils
         je utils_menu
+        cmp AL, 53 ;------------------ Exit
+        je close
+        cmp AL, 73 ;------------------ Exit
+        je close
         jmp principal_menu ;------------------ End of principal menu
 products_menu: ;----------------------------------------------Start of products menu
         print new_line
@@ -458,6 +474,7 @@ products_menu: ;----------------------------------------------Start of products 
         print products_create_msg
         print products_delete_msg
         print products_show_msg
+        print back_option
         call get_pressed_key
         cmp AL, 43 ;------------------ Create
         je products_create
@@ -471,11 +488,17 @@ products_menu: ;----------------------------------------------Start of products 
         je products_show
         cmp AL, 6d ;------------------ Show
         je products_show
+        cmp AL, 52 ;------------------ Back
+        je principal_menu
+        cmp AL, 72 ;------------------ Back
+        je principal_menu
         jmp products_menu ;------------------ End of products menu
 products_create: ;----------------------------------------------Start of products create
         print new_line
         print req_product
 get_code_product: ;--------------------- Get code product
+        int 03
+        print new_line
         print req_code
         get_string in_buffer
         mov DI, offset in_buffer ;------ Validate code product, PENDING need to validate if chars are in acceptable range
@@ -483,7 +506,7 @@ get_code_product: ;--------------------- Get code product
         mov AL, [DI]
         cmp AL, 00
         je get_code_product
-        cmp AL, 06
+        cmp AL, 05
         jb accept_code_product
         jmp get_code_product
 accept_code_product: ;--------------------- Accept code product
@@ -495,6 +518,7 @@ accept_code_product: ;--------------------- Accept code product
         inc SI
         call copy_string
 get_desc_product:
+        print new_line
         print req_desc
         get_string in_buffer
         mov DI, offset in_buffer ;------ Validate desc product, PENDING need to validate if chars are in acceptable range
@@ -502,7 +526,7 @@ get_desc_product:
         mov AL, [DI]
         cmp AL, 00
         je get_desc_product
-        cmp AL, 22
+        cmp AL, 21
         jb accept_desc_product
         jmp get_desc_product
 accept_desc_product: ;--------------------- Accept desc product
@@ -514,6 +538,7 @@ accept_desc_product: ;--------------------- Accept desc product
         inc SI
         call copy_string
 get_price_product:
+        print new_line
         print req_price
         get_string in_buffer
         mov DI, offset in_buffer ;------ Validate price product, PENDING need to validate if chars are in acceptable range
@@ -534,7 +559,7 @@ accept_price_product: ;--------------------- Accept price product
         call copy_string
 
         mov DI, offset product_price
-        call string_to_int
+        call string_to_num
         mov [product_num_price], AX
 
         mov DI, offset product_price
@@ -542,6 +567,7 @@ accept_price_product: ;--------------------- Accept price product
         mov AL, 00
         call memset
 get_stock_product:
+        print new_line
         print req_stock
         get_string in_buffer
         mov DI, offset in_buffer ;------ Validate stock product, PENDING need to validate if chars are in acceptable range
@@ -553,6 +579,7 @@ get_stock_product:
         jb accept_stock_product
         jmp get_stock_product
 accept_stock_product: ;--------------------- Accept stock product
+        print new_line
         mov DI, offset product_stock
         mov SI, offset in_buffer
         inc SI
@@ -562,7 +589,7 @@ accept_stock_product: ;--------------------- Accept stock product
         call copy_string
 
         mov DI, offset product_stock
-        call string_to_int
+        call string_to_num
         mov [product_num_stock], AX
 
         mov DI, offset product_stock
@@ -586,14 +613,51 @@ create_product_file:
 save_product:
         mov [handle_products], AX
         mov BX, [handle_products]
-        ; We go to the end of file
-        mov CX, 0000
         mov DX, 0000
-        mov AH, 42
-        mov AL, 02
+        mov [pointer_tmp], DX
+        ; We go to the end of file
+        ; mov CX, 0000
+        ; mov DX, 0000
+        ; mov AH, 42
+        ; mov AL, 02
+        ; int 21
+find_zero:
+        mov CX, 28
+        mov DX, offset product_tmp
+        mov AH, 3f
         int 21
+        cmp AX, 0000 ;------------------ End of file
+        je write_product
+
+        mov DX, [pointer_tmp] ;----------- We move the pointer
+        add DX, 0028
+        mov [pointer_tmp], DX
+
+        mov DI, offset product_tmp ;-------- Verify if product already exist
+        mov SI, offset product_code
+        mov CX, 0004
+        call compare_string
+        cmp DX, 0001
+        je product_exists
+
+        mov AL, 00      ;------------------ Verify if product is valid
+        cmp [product_tmp], AL
+        jne find_zero
+
+        mov DX, [pointer_tmp] ;------------------ Position the pointer at the beginning of the product
+        sub DX, 0028
+        mov CX, 0000
+        mov BX, [handle_products]
+        mov AX, 4200
+        int 21
+        jmp write_product
+product_exists:
+        print new_line
+        print product_exists_msg
+        jmp not_write_product
+write_product:
         ; We write the product
-        mov CX, 0026
+        mov CX, 0024
         mov DX, offset product_code
         mov AH, 40
         int 21
@@ -601,14 +665,112 @@ save_product:
         mov DX, offset product_num_price
         mov AH, 40
         int 21
+not_write_product:
         ; We close the file
         mov AH, 3e
         int 21
+        ; We clean the variables
+        mov DI, offset product_tmp
+        mov CX, 0028
+        mov AL, 00
+        call memset
+        mov DI, offset product_code
+        mov CX, 0004
+        mov AL, 00
+        call memset
+        mov DI, offset product_desc
+        mov CX, 0020
+        mov AL, 00
+        call memset
         jmp products_menu ;------------------ End of products create
 products_delete: ;----------------------------------------------Start of products delete
+        mov DX, 0000
+        mov [pointer_tmp], DX
+get_code_delete: ;--------------------- Get code product to delete
+        print new_line
+        print req_code
+        get_string in_buffer
+        mov DI, offset in_buffer ;------ Validate code product, PENDING need to validate if chars are in acceptable range
+        inc DI
+        mov AL, [DI]
+        cmp AL, 00
+        je get_code_delete
+        cmp AL, 05
+        jb accept_code_delete
+        jmp get_code_delete
+accept_code_delete: ;--------------------- Accept code product to delete
+        mov AL, 02
+        mov AH, 3d
+        mov DX, offset products_file
+        int 21
+        mov [handle_products], AX
+find_cycle:
+        int 03
+        mov BX, [handle_products]
+        mov CX, 0024
+        mov DX, offset product_code
+        mov AH, 3f
+        int 21
+        mov BX, [handle_products]
+        mov CX, 0004
+        mov DX, offset product_num_price
+        mov AH, 3f
+        int 21
+        cmp AX, 0000 ;------------------ End of file
+        je delete_not_found
+        
+        mov DX, [pointer_tmp] ;----------- We move the pointer
+        add DX, 0028
+        mov [pointer_tmp], DX
+        
+        mov AL, 00      ;------------------ Verify if product is valid
+        cmp [product_code], AL
+        je find_cycle
 
+        mov SI, offset in_buffer ;------ Verify if code is equal
+        add SI, 0002
+        mov DI, offset product_code
+        mov CX, 0004
+        call compare_string
+        cmp DX, 0001
+        je delete_product
+        jmp find_cycle
+delete_product:
+        mov DX, [pointer_tmp] ;------------------ Position the pointer at the beginning of the product
+        sub DX, 0028
+        mov CX, 0000
+        mov BX, [handle_products]
+        mov AX, 4200
+        int 21
+
+        mov CX, 0028 ;------------------ Write 0's to the product
+        mov DX, offset zeros
+        mov AH, 40
+        int 21
+        mov DI, offset product_code
+        mov CX, 0004
+        mov AL, 00
+        call memset
+        print new_line
+        print product_deleted
+        jmp close_delete
+delete_not_found:
+        print new_line
+        print product_not_found
+close_delete:
+        mov AH, 3e
+        int 21
+        jmp products_menu ;------------------ End of products delete
 products_show: ;----------------------------------------------Start of products show
-
+        mov AL, 00
+        mov AH, 3d
+        mov DX, offset products_file
+        int 21
+        mov [handle_products], AX
+        ; SHOW 5 PRODUCTS AND WAIT FOR KEY
+        ;       IF KEY IS 'ENTER' SHOW NEXT 5 PRODUCTS
+        ;       IF KEY IS 'q' RETURN TO PRODUCTS MENU
+        
 sales_menu: ;-------------------------------------------------Start of sales menu
 
 utils_menu: ;------------------------------------------------Start of utils menu
